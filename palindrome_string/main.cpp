58a2e2dc055db5af36678715a9594e4205c24314
ubyhzargam <ubyhzargam@gmail.com>
2023-06-22 19:45:32 +0530

    Update main.cpp

--- palindrome_string/main.cpp
+++ palindrome_string/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+using namespace std;
+int main()
+{
+    char str[50];
+    cout<<"Enter a string to find out whether it is palindrome or not : "<<endl;
+    cin>>str;
+    int l=0,i;
+    for(i=0;i<50;i++)
+    {
+        if(str[i]=='\0')
+            break;
+        l++;
+    }
+    int h=l-1;
+    for( i=0;i<(l)/2;i++)
+    {
+        if(str[i]==str[h])
+        {
+            h--;
+            continue;
+        }
+        else
+        {
+            cout<<"The given string is not palindrome in nature"<<endl;
+            break;
+        }
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    }
+    if(i==(l)/2)
+        cout<<"The given string is Palindrome in nature"<<endl;
     return 0;
 }
